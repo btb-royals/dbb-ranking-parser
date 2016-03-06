dc933b585e775cee08201c217e2c1f53e7801cce
Jochen Kupperschmidt <homework@nwsnet.de>
2016-03-06 04:03:08 +0100

    Moved more functionality to the document module.

--- dbbrankingparser/__init__.py
+++ dbbrankingparser/__init__.py
@@ -13,8 +13,7 @@ The resulting data is structured as a list of dictionaries.
 :License: MIT, see LICENSE for details.
 """
 
-from .conversion import convert_attributes
-from .document import get_rank_values, has_team_withdrawn, select_rank_rows
+from .document import parse
 from .http import fetch_content
 
 
@@ -22,31 +21,3 @@ def load_ranking(url):
     """Fetch the URL's content and yield ranks extracted from it."""
     html = fetch_content(url)
     return parse(html)
-
-
-def parse(html):
-    """Yield ranks extracted from HTML document."""
-    trs = select_rank_rows(html)
-    return _parse_rank_rows(trs)
-
-
-def _parse_rank_rows(trs):
-    """Yield ranks extracted from table rows."""
-    for tr in trs:
-        rank = _parse_rank_row(tr)
-        if rank:
-            yield rank
-
-
-def _parse_rank_row(tr):
-    """Attempt to extract a single rank's properties from a table row."""
-    team_has_withdrawn = has_team_withdrawn(tr)
-
-    values = get_rank_values(tr, team_has_withdrawn)
-
-    if not values:
-        return None
-
-    attributes = convert_attributes(values)
-    attributes['withdrawn'] = team_has_withdrawn
-    return attributes
--- dbbrankingparser/document.py
+++ dbbrankingparser/document.py
@@ -12,6 +12,14 @@ HTML document utilities
 
 from lxml.html import document_fromstring
 
+from .conversion import convert_attributes
+
+
+def parse(html):
+    """Yield ranks extracted from HTML document."""
+    trs = select_rank_rows(html)
+    return parse_rank_rows(trs)
+
 
 def select_rank_rows(html):
     """Return the table rows that are expected to contain rank data."""
@@ -20,14 +28,36 @@ def select_rank_rows(html):
         'body/form/table[@class="sportView"][2]/tr[position() > 1]')
 
 
+def parse_rank_rows(trs):
+    """Yield ranks extracted from table rows."""
+    for tr in trs:
+        rank = parse_rank_row(tr)
+        if rank:
+            yield rank
+
+
+def parse_rank_row(tr):
+    """Attempt to extract a single rank's properties from a table row."""
+    team_has_withdrawn = has_team_withdrawn(tr)
+
+    values = get_rank_values(tr, team_has_withdrawn)
+
+    if not values:
+        return None
+
+    attributes = convert_attributes(values)
+    attributes['withdrawn'] = team_has_withdrawn
+    return attributes
+
+
+def has_team_withdrawn(tr):
+    """Return `True` if the markup indicates that the team has withdrawn."""
+    return bool(tr.xpath('td[2]/nobr/strike'))
+
+
 def get_rank_values(tr, team_has_withdrawn):
     """Return that row's cell values."""
     xpath_expression = 'td/nobr/strike/text()' if team_has_withdrawn \
                        else 'td/nobr/text()'
 
     return tr.xpath(xpath_expression)
-
-
-def has_team_withdrawn(tr):
-    """Return `True` if the markup indicates that the team has withdrawn."""
-    return bool(tr.xpath('td[2]/nobr/strike'))
