65726b4efd7f007f93fc3c91f2317d41a59b9313
Jochen Kupperschmidt <homework@nwsnet.de>
2016-03-06 04:40:02 +0100

    Moved entry points to a new 'main' module in preparation of future modules that want to include those functions.

--- dbbrankingparser/__init__.py
+++ dbbrankingparser/__init__.py
@@ -16,19 +16,4 @@ season, but not those of the past.
 :License: MIT, see LICENSE for details.
 """
 
-from .document import parse
-from .http import assemble_url, fetch_content
-
-
-def load_ranking_for_league(league_id):
-    """Fetch the HTML ranking for that league and yield ranks extracted
-    from it.
-    """
-    url = assemble_url(league_id)
-    return load_ranking_from_url(url)
-
-
-def load_ranking_from_url(url):
-    """Fetch the URL's content and yield ranks extracted from it."""
-    html = fetch_content(url)
-    return parse(html)
+from .main import load_ranking_for_league, load_ranking_from_url
