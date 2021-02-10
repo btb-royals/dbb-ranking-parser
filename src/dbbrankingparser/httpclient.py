16d0aeb218df0c0dbccf06af107f2c00d1c7a7a7
Jochen Kupperschmidt <homework@nwsnet.de>
2021-02-10 04:19:50 +0100

    Update user agent string

--- src/dbbrankingparser/httpclient.py
+++ src/dbbrankingparser/httpclient.py
@@ -12,8 +12,7 @@ from urllib.request import Request, urlopen
 
 
 USER_AGENT: str = (
-    'Mozilla/5.0 (X11; Linux x86_64; rv:38.0) '
-    'Gecko/20100101 Firefox/38.0 Iceweasel/38.6.0'
+    'Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0'
 )
 
 
